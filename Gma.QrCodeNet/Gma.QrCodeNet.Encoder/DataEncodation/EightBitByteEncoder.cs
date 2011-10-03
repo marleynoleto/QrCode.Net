1dde314979fbe45881c4a7cc8ab549fb080024eb
SND\Silverlancer_cp <SND\Silverlancer_cp@2abf6ff4-378a-4344-b6f7-2bcd8b316ba9>
2011-10-03 03:17:22 +0000

    Update EightBitByteEncoder.cs Using encoding from .Net instead of out of box functionality.
    git-svn-id: https://QrCodeNet.svn.codeplex.com/svn@5089 2abf6ff4-378a-4344-b6f7-2bcd8b316ba9

--- Gma.QrCodeNet/Gma.QrCodeNet.Encoder/DataEncodation/EightBitByteEncoder.cs
+++ Gma.QrCodeNet/Gma.QrCodeNet.Encoder/DataEncodation/EightBitByteEncoder.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 using com.google.zxing.qrcode.encoder;
 
-/*
- * Created by SharpDevelop.
- * User: SilverLancer
- * Date: 30/09/2011
- * Time: 11:54 a.m.
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-
 namespace Gma.QrCodeNet.Encoding.DataEncodation
 {
 	/// <summary>
@@ -30,82 +21,53 @@ namespace Gma.QrCodeNet.Encoding.DataEncodation
 		internal override BitVector GetDataBits(string content)
         {
 			BitVector dataBits = new BitVector();
-			for(int i = 0; i < content.Length; i++)
-			{
-				int value = GetEightBitByteValue(content, i);
-				//Reference ISO/IEC 18004:2000(E) B=4+C+8D (P.24) 
-				//D=Number of imput data char. C=number of char count indicator(table3) Method:GetBitCountInCharCountIndicator
-				dataBits.appendBits(value, 8);	//EightBitByte different to Num and AlphaNum. bitCount for each Char is constant 8;
+			
+			byte[] contentBytes;
+			int contentLength = content.Length;
+			try {
+				contentBytes = System.Text.Encoding.GetEncoding("Shift_JIS").GetBytes(content);
+			} catch (Exception e) {
+				
+				throw e;
 			}
+			
+			if(contentBytes.Length == contentLength)
+			{
+				for(int i = 0; i < contentLength; i++)
+				{
+					dataBits.appendBits(contentBytes[i], 8);	//EightBitByte different to Num and AlphaNum. bitCount for each Char is constant 8;
+				}
+			}
+			else
+				throw new System.ArgumentException("Content contain non JIS8 char");
+			
 			return dataBits;
 		}
 		
 		internal override bool TryGetDataBits(string content, ref BitVector dataBits)
         {
-			//dataBits = new BitVector();
-			for(int i = 0; i < content.Length; i++)
+			byte[] contentBytes;
+			int contentLength = content.Length;
+			try {
+				contentBytes = System.Text.Encoding.GetEncoding("Shift_JIS").GetBytes(content);
+			} catch (Exception) {
+				
+				return false;
+			}
+			
+			if(contentBytes.Length == contentLength)
 			{
-				int value;
-				if(!TryGetEightBitByteValue(content, i, out value))
-					return false;
-				//Reference ISO/IEC 18004:2000(E) B=4+C+8D (P.24) 
-				//D=Number of imput data char. C=number of char count indicator(table3) Method:GetBitCountInCharCountIndicator
-				dataBits.appendBits(value, 8);	//EightBitByte different to Num and AlphaNum. bitCount for each Char is constant 8;
+				for(int i = 0; i < contentLength; i++)
+				{
+					dataBits.appendBits(contentBytes[i], 8);
+				}
+				return true;
 			}
-			return true;
-		}
-		
-		
-		private int GetEightBitByteValue(string content, int startIndex)
-        {
-			int value = content[startIndex];
+			else
+				return false;
 			
-			switch (value) {
-				case 0x00A5:
-					return 0x5C;	//0x00A5 is JP Yan mark "¥"
-				case 0x203E:
-					return 0x7E;	//0x203E = "‾" According to Table 6 and JIS X 0201 to Unicode table Url:"http://charset.7jp.net/jis0201.html"
-				case 0x5C:
-					throw new FormatException();
-				case 0x7E:
-					throw new FormatException();
-				default:
-					if(value >= 0x00 && value <= 0x7F)	// 0x00 > 0x7F same as Table 6
-						return value;
-					else if(value >= 0xFF61 && value <= 0xFF9F)  //0xA1 > 0xDF from Table 6 increased by 0xFEC0 on Unicode 16
-						return value - 0xFEC0;
-					else
-						throw new FormatException();
-			}
 		}
 		
-		private bool TryGetEightBitByteValue(string content, int startIndex, out int value)
-        {
-			value = content[startIndex];
-			
-			switch (value) {
-				case 0x00A5:
-					value = 0x5C;	//0x00A5 is JP Yan mark "¥"
-					return true;
-				case 0x203E:
-					value = 0x7E;	//0x203E = "‾" According to Table 6 and JIS X 0201 to Unicode table Url:"http://charset.7jp.net/jis0201.html"
-					return true;
-				case 0x5C:
-					return false;
-				case 0x7E:
-					return false;
-				default:
-					if(value >= 0x00 && value <= 0x7F)	// 0x00 > 0x7F same as Table 6
-						return true;
-					else if(value >= 0xFF61 && value <= 0xFF9F)  //0xA1 > 0xDF from Table 6 increased by 0xFEC0 on Unicode 16
-					{
-						value -= 0xFEC0;
-						return true;
-					}
-					else
-						return false;
-			}
-		}
 		
 		/// <summary>
         /// Defines the length of the Character Count Indicator, 
