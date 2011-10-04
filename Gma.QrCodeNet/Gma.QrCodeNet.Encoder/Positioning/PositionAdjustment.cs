8eae7170be5261dc537f670baded0a26c20a9b53
SND\gmamaladze_cp <SND\gmamaladze_cp@2abf6ff4-378a-4344-b6f7-2bcd8b316ba9>
2011-10-04 08:34:29 +0000

    Refactoring position detection and position adjustment patterns.
    git-svn-id: https://QrCodeNet.svn.codeplex.com/svn@5181 2abf6ff4-378a-4344-b6f7-2bcd8b316ba9

---
+++ Gma.QrCodeNet/Gma.QrCodeNet.Encoder/Positioning/PositionAdjustment.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gma.QrCodeNet.Encoding.Positioning
+{
+    class PositionAdjustment
+    {
+        private static readonly int[][] s_PositionAdjustmentPattern = new[]
+                                                                          {
+                                                                              new [] { 1, 1, 1, 1, 1 }, 
+                                                                              new [] { 1, 0, 0, 0, 1 }, 
+                                                                              new [] { 1, 0, 1, 0, 1 }, 
+                                                                              new [] { 1, 0, 0, 0, 1 }, 
+                                                                              new [] { 1, 1, 1, 1, 1 }
+                                                                          };
+
+
+        private static readonly byte[][] s_PositionAdjustmentPatternCoordinatesByVersion = new []
+                                                                                         {
+                                                                                             null,
+                                                                                             new byte[] {} , 
+                                                                                             new byte[] { 6, 18}, 
+                                                                                             new byte[] { 6, 22 }, 
+                                                                                             new byte[] { 6, 26 }, 
+                                                                                             new byte[] { 6, 30 }, 
+                                                                                             new byte[] { 6, 34 }, 
+                                                                                             new byte[] { 6, 22, 38 }, 
+                                                                                             new byte[] { 6, 24, 42 }, 
+                                                                                             new byte[] { 6, 26, 46 }, 
+                                                                                             new byte[] { 6, 28, 50 }, 
+                                                                                             new byte[] { 6, 30, 54 }, 
+                                                                                             new byte[] { 6, 32, 58 }, 
+                                                                                             new byte[] { 6, 34, 62 }, 
+                                                                                             new byte[] { 6, 26, 46, 66 }, 
+                                                                                             new byte[] { 6, 26, 48, 70 }, 
+                                                                                             new byte[] { 6, 26, 50, 74 }, 
+                                                                                             new byte[] { 6, 30, 54, 78 }, 
+                                                                                             new byte[] { 6, 30, 56, 82 }, 
+                                                                                             new byte[] { 6, 30, 58, 86 }, 
+                                                                                             new byte[] { 6, 34, 62, 90 }, 
+                                                                                             new byte[] { 6, 28, 50, 72, 94 }, 
+                                                                                             new byte[] { 6, 26, 50, 74, 98 }, 
+                                                                                             new byte[] { 6, 30, 54, 78, 102 }, 
+                                                                                             new byte[] { 6, 28, 54, 80, 106 }, 
+                                                                                             new byte[] { 6, 32, 58, 84, 110 }, 
+                                                                                             new byte[] { 6, 30, 58, 86, 114 }, 
+                                                                                             new byte[] { 6, 34, 62, 90, 118 }, 
+                                                                                             new byte[] { 6, 26, 50, 74, 98, 122 }, 
+                                                                                             new byte[] { 6, 30, 54, 78, 102, 126 }, 
+                                                                                             new byte[] { 6, 26, 52, 78, 104, 130 }, 
+                                                                                             new byte[] { 6, 30, 56, 82, 108, 134 }, 
+                                                                                             new byte[] { 6, 34, 60, 86, 112, 138 }, 
+                                                                                             new byte[] { 6, 30, 58, 86, 114, 142 }, 
+                                                                                             new byte[] { 6, 34, 62, 90, 118, 146 }, 
+                                                                                             new byte[] { 6, 30, 54, 78, 102, 126, 150 }, 
+                                                                                             new byte[] { 6, 24, 50, 76, 102, 128, 154 }, 
+                                                                                             new byte[] { 6, 28, 54, 80, 106, 132, 158 }, 
+                                                                                             new byte[] { 6, 32, 58, 84, 110, 136, 162 }, 
+                                                                                             new byte[] { 6, 26, 54, 82, 110, 138, 166 }, 
+                                                                                             new byte[] { 6, 30, 58, 86, 114, 142, 170 }
+                                                                                         };
+
+        internal PositionAdjustment(byte[] coordinates)
+        {
+            
+        }
+
+        private static void embedPositionAdjustmentPattern(int xStart, int yStart, TriStateMatrix matrix)
+        {
+            for (int y = 0; y < 5; ++y)
+            {
+                for (int x = 0; x < 5; ++x)
+                {
+                    matrix.Set(yStart + y, xStart + x, (sbyte)s_PositionAdjustmentPattern[y][x]==1);
+                }
+            }
+        }
+
+        internal static void maybeEmbedPositionAdjustmentPatterns(int version, TriStateMatrix matrix)
+        {
+            byte[] coordinates = s_PositionAdjustmentPatternCoordinatesByVersion[version];
+            foreach (byte x in coordinates)
+            {
+                foreach (byte y in coordinates)
+                {
+                    // If the cell is unset, we embed the position adjustment pattern here.
+                    if (!matrix.IsUsed(x, y))
+                    {
+                        // -2 is necessary since the x/y coordinates point to the center of the pattern, not the
+                        // left top corner.
+                        embedPositionAdjustmentPattern(y - 2, x - 2, matrix);
+                    }
+                }
+            }
+        }
+    }
+}
--- Gma.QrCodeNet/Gma.QrCodeNet.Encoder/Positioning/PositionDetection.cs
+++ Gma.QrCodeNet/Gma.QrCodeNet.Encoder/Positioning/PositionDetection.cs
@@ -4,23 +4,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
 {
     internal static class PositioninngPatternBuilder
     {
-
-        private static readonly sbyte[][] s_PatternStamp = new[]
-                                                                           {
-                                                                               new sbyte [] {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                                               new sbyte[] {0, 1, 1, 1, 1, 1, 1, 1, 0},
-                                                                               new sbyte[] {0, 1, 0, 0, 0, 0, 0, 1, 0},
-                                                                               new sbyte[] {0, 1, 0, 1, 1, 1, 0, 1, 0},
-                                                                               new sbyte[] {0, 1, 0, 1, 1, 1, 0, 1, 0},
-                                                                               new sbyte[] {0, 1, 0, 1, 1, 1, 0, 1, 0},
-                                                                               new sbyte[] {0, 1, 0, 0, 0, 0, 0, 1, 0},
-                                                                               new sbyte[] {0, 1, 1, 1, 1, 1, 1, 1, 0},
-                                                                               new sbyte[] {0, 0, 0, 0, 0, 0, 0, 0, 0}
-                                                                           };
-
-
-
-        private static readonly int[][] POSITION_DETECTION_PATTERN = new int[][]
+        private static readonly int[][] s_PositionDetectionPattern = new int[][]
                                                                          {
                                                                              new int[] { 1, 1, 1, 1, 1, 1, 1 }, 
                                                                              new int[] { 1, 0, 0, 0, 0, 0, 1 }, 
@@ -31,13 +15,10 @@ namespace Gma.QrCodeNet.Encoding.Positioning
                                                                              new int[] { 1, 1, 1, 1, 1, 1, 1 }
                                                                          };
 
-        private static readonly int[][] HORIZONTAL_SEPARATION_PATTERN = new int[][] { new int[] { 0, 0, 0, 0, 0, 0, 0, 0 } };
+        private static readonly int[][] s_HorizontalSeparationPattern = new int[][] { new int[] { 0, 0, 0, 0, 0, 0, 0, 0 } };
 
-        private static readonly int[][] VERTICAL_SEPARATION_PATTERN = new int[][] { new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 } };
+        private static readonly int[][] s_VerticalSeparationPattern = new int[][] { new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, new int[] { 0 } };
 
-        private static readonly int[][] POSITION_ADJUSTMENT_PATTERN = new int[][] { new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 0, 0, 0, 1 }, new int[] { 1, 0, 1, 0, 1 }, new int[] { 1, 0, 0, 0, 1 }, new int[] { 1, 1, 1, 1, 1 } };
-
-        private static readonly int[][] POSITION_ADJUSTMENT_PATTERN_COORDINATE_TABLE = new int[][] { new int[] { -1, -1, -1, -1, -1, -1, -1 }, new int[] { 6, 18, -1, -1, -1, -1, -1 }, new int[] { 6, 22, -1, -1, -1, -1, -1 }, new int[] { 6, 26, -1, -1, -1, -1, -1 }, new int[] { 6, 30, -1, -1, -1, -1, -1 }, new int[] { 6, 34, -1, -1, -1, -1, -1 }, new int[] { 6, 22, 38, -1, -1, -1, -1 }, new int[] { 6, 24, 42, -1, -1, -1, -1 }, new int[] { 6, 26, 46, -1, -1, -1, -1 }, new int[] { 6, 28, 50, -1, -1, -1, -1 }, new int[] { 6, 30, 54, -1, -1, -1, -1 }, new int[] { 6, 32, 58, -1, -1, -1, -1 }, new int[] { 6, 34, 62, -1, -1, -1, -1 }, new int[] { 6, 26, 46, 66, -1, -1, -1 }, new int[] { 6, 26, 48, 70, -1, -1, -1 }, new int[] { 6, 26, 50, 74, -1, -1, -1 }, new int[] { 6, 30, 54, 78, -1, -1, -1 }, new int[] { 6, 30, 56, 82, -1, -1, -1 }, new int[] { 6, 30, 58, 86, -1, -1, -1 }, new int[] { 6, 34, 62, 90, -1, -1, -1 }, new int[] { 6, 28, 50, 72, 94, -1, -1 }, new int[] { 6, 26, 50, 74, 98, -1, -1 }, new int[] { 6, 30, 54, 78, 102, -1, -1 }, new int[] { 6, 28, 54, 80, 106, -1, -1 }, new int[] { 6, 32, 58, 84, 110, -1, -1 }, new int[] { 6, 30, 58, 86, 114, -1, -1 }, new int[] { 6, 34, 62, 90, 118, -1, -1 }, new int[] { 6, 26, 50, 74, 98, 122, -1 }, new int[] { 6, 30, 54, 78, 102, 126, -1 }, new int[] { 6, 26, 52, 78, 104, 130, -1 }, new int[] { 6, 30, 56, 82, 108, 134, -1 }, new int[] { 6, 34, 60, 86, 112, 138, -1 }, new int[] { 6, 30, 58, 86, 114, 142, -1 }, new int[] { 6, 34, 62, 90, 118, 146, -1 }, new int[] { 6, 30, 54, 78, 102, 126, 150 }, new int[] { 6, 24, 50, 76, 102, 128, 154 }, new int[] { 6, 28, 54, 80, 106, 132, 158 }, new int[] { 6, 32, 58, 84, 110, 136, 162 }, new int[] { 6, 26, 54, 82, 110, 138, 166 }, new int[] { 6, 30, 58, 86, 114, 142, 170 } };
 
         // Embed basic patterns. On success, modify the matrix and return true.
         // The basic patterns are:
@@ -53,7 +34,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
             embedDarkDotAtLeftBottomCorner(matrix);
 
             // Position adjustment patterns appear if version >= 2.
-            maybeEmbedPositionAdjustmentPatterns(version, matrix);
+            PositionAdjustment.maybeEmbedPositionAdjustmentPatterns(version, matrix);
             // Timing patterns should be embedded after position adj. patterns.
             embedTimingPatterns(matrix);
         }
@@ -91,7 +72,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
         {
             for (int x = 0; x < 8; ++x)
             {
-                matrix.Set(yStart, xStart + x, (sbyte)HORIZONTAL_SEPARATION_PATTERN[0][x]==1);
+                matrix.Set(yStart, xStart + x, (sbyte)s_HorizontalSeparationPattern[0][x]==1);
             }
         }
 
@@ -99,23 +80,13 @@ namespace Gma.QrCodeNet.Encoding.Positioning
         {
             for (int y = 0; y < 7; ++y)
             {
-                matrix.Set(yStart + y, xStart, (sbyte)VERTICAL_SEPARATION_PATTERN[y][0]==1);
+                matrix.Set(yStart + y, xStart, (sbyte)s_VerticalSeparationPattern[y][0]==1);
             }
         }
 
         // Note that we cannot unify the function with embedPositionDetectionPattern() despite they are
         // almost identical, since we cannot write a function that takes 2D arrays in different sizes in
         // C/C++. We should live with the fact.
-        private static void embedPositionAdjustmentPattern(int xStart, int yStart, TriStateMatrix matrix)
-        {
-            for (int y = 0; y < 5; ++y)
-            {
-                for (int x = 0; x < 5; ++x)
-                {
-                    matrix.Set(yStart + y, xStart + x, (sbyte)POSITION_ADJUSTMENT_PATTERN[y][x]==1);
-                }
-            }
-        }
 
         private static void embedPositionDetectionPattern(int xStart, int yStart, TriStateMatrix matrix)
         {
@@ -123,7 +94,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
             {
                 for (int x = 0; x < 7; ++x)
                 {
-                    matrix.Set(yStart + y, xStart + x, (sbyte)POSITION_DETECTION_PATTERN[y][x]==1);
+                    matrix.Set(yStart + y, xStart + x, (sbyte)s_PositionDetectionPattern[y][x]==1);
                 }
             }
         }
@@ -132,7 +103,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
         private static void embedPositionDetectionPatternsAndSeparators(TriStateMatrix matrix)
         {
             // Embed three big squares at corners.
-            int pdpWidth = POSITION_DETECTION_PATTERN[0].Length;
+            int pdpWidth = s_PositionDetectionPattern[0].Length;
             // Left top corner.
             embedPositionDetectionPattern(0, 0, matrix);
             // Right top corner.
@@ -141,7 +112,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
             embedPositionDetectionPattern(0, matrix.Width - pdpWidth, matrix);
 
             // Embed horizontal separation patterns around the squares.
-            int hspWidth = HORIZONTAL_SEPARATION_PATTERN[0].Length;
+            int hspWidth = s_HorizontalSeparationPattern[0].Length;
             // Left top corner.
             embedHorizontalSeparationPattern(0, hspWidth - 1, matrix);
             // Right top corner.
@@ -150,7 +121,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning
             embedHorizontalSeparationPattern(0, matrix.Width - hspWidth, matrix);
 
             // Embed vertical separation patterns around the squares.
-            int vspSize = VERTICAL_SEPARATION_PATTERN.Length;
+            int vspSize = s_VerticalSeparationPattern.Length;
             // Left top corner.
             embedVerticalSeparationPattern(vspSize, 0, matrix);
             // Right top corner.
@@ -160,35 +131,5 @@ namespace Gma.QrCodeNet.Encoding.Positioning
         }
 
         // Embed position adjustment patterns if need be.
-        private static void maybeEmbedPositionAdjustmentPatterns(int version, TriStateMatrix matrix)
-        {
-            if (version < 2)
-            {
-                // The patterns appear if version >= 2
-                return;
-            }
-            int index = version - 1;
-            int[] coordinates = POSITION_ADJUSTMENT_PATTERN_COORDINATE_TABLE[index];
-            int numCoordinates = POSITION_ADJUSTMENT_PATTERN_COORDINATE_TABLE[index].Length;
-            for (int i = 0; i < numCoordinates; ++i)
-            {
-                for (int j = 0; j < numCoordinates; ++j)
-                {
-                    int y = coordinates[i];
-                    int x = coordinates[j];
-                    if (x == -1 || y == -1)
-                    {
-                        continue;
-                    }
-                    // If the cell is unset, we embed the position adjustment pattern here.
-                    if (!matrix.IsUsed(y, x))
-                    {
-                        // -2 is necessary since the x/y coordinates point to the center of the pattern, not the
-                        // left top corner.
-                        embedPositionAdjustmentPattern(x - 2, y - 2, matrix);
-                    }
-                }
-            }
-        }
     }
 }
