03b6ccbea35d8d590bfdd0190a10023a71a83d0e
SND\iMarti_cp <SND\iMarti_cp@2abf6ff4-378a-4344-b6f7-2bcd8b316ba9>
2012-03-26 21:05:10 +0000

    Corrected the rare tiny gaps between dark modules by making neighbors overlap. Added a second drawing technique using the PostScript 'image' or 'colorimage' command.
    git-svn-id: https://QrCodeNet.svn.codeplex.com/svn@13910 2abf6ff4-378a-4344-b6f7-2bcd8b316ba9

--- Gma.QrCodeNet/Gma.QrCodeNet.Encoder/Windows/Render/EncapsulatedPostScriptRenderer.cs
+++ Gma.QrCodeNet/Gma.QrCodeNet.Encoder/Windows/Render/EncapsulatedPostScriptRenderer.cs
@@ -16,6 +16,8 @@ namespace Gma.QrCodeNet.Encoding.Windows.Render
 
 		private QuietZoneModules m_QuietZoneModules;
 
+		private EpsModuleDrawingTechnique m_DrawingTechnique;
+
 		/// <summary>
 		/// Initializes a Encapsulated PostScript renderer.
 		/// </summary>
@@ -29,6 +31,7 @@ namespace Gma.QrCodeNet.Encoding.Windows.Render
 			m_DarkColor = darkColor;
 			m_LightColor = lightColor;
 			m_QuietZoneModules = quietZoneModules;
+			m_DrawingTechnique = EpsModuleDrawingTechnique.Squares;
 		}
 
 		/// <summary>
@@ -38,6 +41,32 @@ namespace Gma.QrCodeNet.Encoding.Windows.Render
 		/// <param name="moduleSize">Size in points (1 inch contains 72 point in PostScript) of a module</param>
 		/// <param name="stream">Output text stream</param>
 		public void WriteToStream(BitMatrix matrix, double moduleSize, StreamWriter stream)
+		{
+			OutputHeader(matrix, moduleSize, stream);
+			OutputBackground(matrix, stream);
+
+			switch (m_DrawingTechnique)
+			{
+				case EpsModuleDrawingTechnique.Squares:
+					DrawSquares(matrix, stream);
+					break;
+				case EpsModuleDrawingTechnique.Image:
+					DrawImage(matrix, stream);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException("DrawingTechnique");
+			}
+
+			OutputFooter(stream);
+		}
+
+		/// <summary>
+		/// Outputs the EPS header with mandatory declarations, variable declarations and function definitions.
+		/// </summary>
+		/// <param name="matrix">The matrix to be rendered</param>
+		/// <param name="moduleSize">Size in points (1 inch contains 72 point in PostScript) of a module</param>
+		/// <param name="stream">Output text stream</param>
+		private void OutputHeader(BitMatrix matrix, double moduleSize, StreamWriter stream)
 		{
 			string strHeader = @"%!PS-Adobe-3.0 EPSF-3.0
 %%Creator: Gma.QrCodeNet
@@ -54,10 +83,15 @@ namespace Gma.QrCodeNet.Encoding.Windows.Render
 /q {{ {5} }} def
 /s {{ {6} }} def
 /W {{ w q q add add }} def
-/H {{ h q q add add }} def
-% Define the box function taking X and Y coordinates of the top left corner and filling a 1 point large square
-/b {{ newpath moveto 1 0 rlineto 0 1 rlineto -1 0 rlineto closepath fill }} def
-%%EndProlog
+/H {{ h q q add add }} def";
+
+			string strBoxFunctions = @"% Define the box functions taking X and Y coordinates of the top left corner and filling a 1 point large square
+/b { newpath moveto 1 0 rlineto 0 1 rlineto -1 0 rlineto closepath fill } def
+/br { newpath moveto 1.01 0 rlineto 0 1 rlineto -1.01 0 rlineto closepath fill } def
+/bb { newpath moveto 1 0 rlineto 0 1.01 rlineto -1 0 rlineto closepath fill } def
+/brb { newpath moveto 1.01 0 rlineto 0 1 rlineto -0.01 0 rlineto 0 0.01 rlineto -1 0 rlineto closepath fill } def";
+
+			string strHeaderEnd = @"%%EndProlog
 %%Page: 1 1
 
 % Save the current state
@@ -66,25 +100,6 @@ save
 % Invert the Y axis
 0 W s mul translate
 s s neg scale";
-
-			string strBackground = @"
-% Create the background
-{0} 255 div {1} 255 div {2} 255 div setrgbcolor
-newpath 0 0 moveto W 0 rlineto 0 H rlineto W neg 0 rlineto closepath fill";
-
-			string strSquaresHeader = @"
-% Draw squares
-{0} 255 div {1} 255 div {2} 255 div setrgbcolor
-q q translate";
-
-			string strFooter = @"
-% Restore the initial state
-restore showpage
-%
-% End of page
-%
-%%Trailer
-%%EOF";
 
 			stream.WriteLine(string.Format(strHeader,
 				DateTime.UtcNow,
@@ -96,24 +111,121 @@ restore showpage
 				(int)m_QuietZoneModules,
 				moduleSize.ToString(CultureInfo.InvariantCulture.NumberFormat)));
 
+			if (m_DrawingTechnique == EpsModuleDrawingTechnique.Squares)
+				stream.WriteLine(strBoxFunctions);
+
+			stream.WriteLine(strHeaderEnd);
+		}
+
+		/// <summary>
+		/// Outputs the background unless it is defined as transparent. The background is used for light modules and quiet zone.
+		/// </summary>
+		/// <param name="matrix">The matrix to be rendered</param>
+		/// <param name="stream">Output text stream</param>
+		private void OutputBackground(BitMatrix matrix, StreamWriter stream)
+		{
+			string strBackground = @"
+% Create the background
+{0} 255 div {1} 255 div {2} 255 div setrgbcolor
+newpath 0 0 moveto W 0 rlineto 0 H rlineto W neg 0 rlineto closepath fill";
+
 			if (LightColor != Color.Transparent)
 				stream.WriteLine(string.Format(strBackground,
 					LightColor.R,
 					LightColor.G,
 					LightColor.B));
+		}
 
-			#region Draw squares for each dark module
+		/// <summary>
+		/// Draw a square for each dark module
+		/// </summary>
+		/// <param name="matrix">The matrix to be rendered</param>
+		/// <param name="stream">Output text stream</param>
+		private void DrawSquares(BitMatrix matrix, StreamWriter stream)
+		{
+			string strSquaresHeader = @"
+% Draw squares
+{0} 255 div {1} 255 div {2} 255 div setrgbcolor
+q q translate";
+
 			stream.WriteLine(string.Format(strSquaresHeader,
 				DarkColor.R,
 				DarkColor.G,
 				DarkColor.B));
 
 			for (int y = 0; y < matrix.Height; ++y)
+			{
 				for (int x = 0; x < matrix.Width; ++x)
-					if (matrix[x, y])
+				{
+					if (matrix[x, y]) // Only draw dark modules
+					{
+						bool bHasRightNeighbor = x + 1 < matrix.Width && matrix[x + 1, y];
+						bool bHasBottomNeighbor = y + 1 < matrix.Height && matrix[x, y + 1];
+						// If the dark module has a dark neighbor on the right, make the two boxed overlap 1% by calling "br" instead of simply "b"
+						// Same applies for bottom neighbor with "bb" command, or "brb" for right and bottom dark neighbors.
+						// Overlapping two modules avoids having tiny gaps appearing on certain zoom scales or output devices.
+
 						// Output the coordinates of the upper left corner and call to the box function
-						stream.WriteLine(string.Format("{0} {1} b", x, y));
-			#endregion
+						stream.WriteLine(string.Format("{0} {1} b{2}{3}", x, y, bHasRightNeighbor ? "r" : "", bHasBottomNeighbor ? "b" : ""));
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Use the 'image' or 'colorimage' PostScript command to render modules
+		/// </summary>
+		/// <param name="matrix">The matrix to be rendered</param>
+		/// <param name="stream">Output text stream</param>
+		private void DrawImage(BitMatrix matrix, StreamWriter stream)
+		{
+			bool bGrayScale =
+				LightColor.R == LightColor.G &&
+				LightColor.G == LightColor.B &&
+				DarkColor.R == DarkColor.G &&
+				DarkColor.G == DarkColor.B;
+			string strLightColor;
+			string strDarkColor;
+			if (bGrayScale)
+			{
+				strLightColor = LightColor.R.ToString("{x2}");
+				strDarkColor = DarkColor.R.ToString("{x2}");
+			}
+			else
+			{
+				strLightColor = string.Format("{0:x2}{1:x2}{2:x2}", LightColor.R, LightColor.G, LightColor.B);
+				strDarkColor = string.Format("{0:x2}{1:x2}{2:x2}", DarkColor.R, DarkColor.G, DarkColor.B);
+			}
+
+			stream.WriteLine(@"% Draw squares
+q q translate
+w h scale
+w h 8 [w 0 0 h 0 0]
+{<");
+			for (int y = 0; y < matrix.Height; ++y)
+			{
+				for (int x = 0; x < matrix.Width; ++x)
+					stream.Write(matrix[x, y] ? strDarkColor : strLightColor);
+				stream.WriteLine();
+			}
+			stream.WriteLine(">}");
+			stream.WriteLine(bGrayScale ? "image" : "false 3 colorimage");
+		}
+
+		/// <summary>
+		/// Outputs the mandatory EPS footer.
+		/// </summary>
+		/// <param name="stream">Output text stream</param>
+		private void OutputFooter(StreamWriter stream)
+		{
+			string strFooter = @"
+% Restore the initial state
+restore showpage
+%
+% End of page
+%
+%%Trailer
+%%EOF";
 
 			stream.Write(strFooter);
 		}
@@ -164,5 +276,27 @@ restore showpage
 				m_QuietZoneModules = value;
 			}
 		}
+
+		/// <summary>
+		/// Selection of the technique used to draw the modules. 'Squares' draws vector squares one by one; 'Image' uses the 'image' or 'colorimage' PostScript command.
+		/// 'Squares' supports transparency of light modules and often has smaller file size for color QR Codes, but can be rendered with tiny spacing between contigus dark modules.
+		/// 'Image' might be faster to render on some devices as it is a single command.
+		/// </summary>
+		public EpsModuleDrawingTechnique DrawingTechnique
+		{
+			get
+			{
+				return m_DrawingTechnique;
+			}
+			set
+			{
+				m_DrawingTechnique = value;
+			}
+		}
 	}
+	public enum EpsModuleDrawingTechnique
+	{
+		Squares,
+		Image
+	};
 }
